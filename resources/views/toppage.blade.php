910724b9c4da2c29f18089159f6e8fe6ff7ef594
curogihu <curoas@yahoo.co.jp>
2015-12-27 04:51:34 -0800

    Reset because of broken site

--- resources/views/toppage.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.default')
-
-@section('title', 'LoL Trend Research')
-
-@section('menuItem1', 'When buy')
-@section('menuItem2', 'When killed')
-@section('menuItem3', 'Where lane')
-@section('menuItem4', 'How many CS')
