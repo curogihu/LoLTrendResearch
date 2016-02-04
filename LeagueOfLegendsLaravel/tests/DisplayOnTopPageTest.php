f805e68029e5606791d99c618aff8870bd9f3c12
curogihu <curoas@yahoo.co.jp>
2016-02-03 23:42:27 -0800

    Add tests for the top page

--- LeagueOfLegendsLaravel/tests/DisplayOnTopPageTest.php
+++ LeagueOfLegendsLaravel/tests/DisplayOnTopPageTest.php
@@ -14,6 +14,23 @@ class DisplayOnTopPageTest extends TestCase
     public function testExample()
     {
       $response = $this->call('GET', '/');
+
+      $this->checkTitle($response);
+      $this->checkItems($response);
+    }
+
+
+    private function checkTitle($response)
+    {
       $this->assertRegexp('/<title>LoL Trend Research<\/title>/', $response->getContent());
+    }
+
+    private function checkItems($response)
+    {
+      $this->assertRegexp('/<a href="\/whenbuy" class="menuItem">When buy<\/a>/', $response->getContent());
+      $this->assertRegexp('/<a href="\/whenkilled" class="menuItem">When killed<\/a>/', $response->getContent());
+      $this->assertRegexp('/<a href="\/wherelane" class="menuItem">Where lane<\/a>/', $response->getContent());
+      $this->assertRegexp('/<a href="\/howmanycs" class="menuItem">How many CS<\/a>/', $response->getContent());
+      $this->assertRegexp('/<a href="\/form" class="menuItem">Search<\/a>/', $response->getContent());
     }
 }
